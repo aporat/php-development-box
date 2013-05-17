0fd69146eee50309c1193d98b86b05b8df4e866b
Adar Porat <hadar@mybuilding.org>
2013-05-17 17:17:49 -0400

    updating main cookbook to support ubuntu

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -1,3 +1,10 @@
+node.set["apache"]["user"]  = "vagrant"
+node.set["apache"]["group"] = "vagrant"
+
+node.set['mysql']['server_root_password'] = "root"
+node.set['mysql']['server_debian_password'] = "root"
+node.set['mysql']['server_repl_password'] = "root"
+
 node.set["app"]["php_timezone"] = "UTC"
 
 node.set["apache"]["user"]  = "vagrant"
@@ -8,17 +15,41 @@ node.set["phpmyadmin"]["version"]  = "4.0.0"
 node.set['mysql']['server_root_password'] = "root"
 node.set['mysql']['old_passwords'] = 1
 
-
-include_recipe "build-essential"
-include_recipe "apache2::default"
-include_recipe "apache2::mod_ssl"
-include_recipe "apache2::mod_rewrite"
-include_recipe "openssl::default"
-
 include_recipe "php54"
-include_recipe "composer"
 
-include_recipe "phpmyadmin::default"
+case node['platform_family']
+  when "rhel", "fedora", "suse"
+  
+  when "debian"
+    include_recipe "apt"
+end
+
+include_recipe "networking_basic"
+
+include_recipe "apache2"
+include_recipe "apache2::mod_php5"
+include_recipe "apache2::mod_rewrite"
+include_recipe "apache2::mod_ssl"
+include_recipe "mysql::server"
+include_recipe "openssl"
+
+
+case node['platform_family']
+  when "rhel", "fedora", "suse"
+  
+  bash "disable_firewall" do
+    only_if "chkconfig --list | grep iptables"
+    code <<-EOH
+      service iptables stop
+      chkconfig iptables off
+    EOH
+  end
+
+  # Vagrant has a speical kernel, which cannot be updated with yum
+  execute "echo 'exclude=kernel*' >> /etc/yum.conf"
+end
+
+include_recipe "phpmyadmin"
 
 phpmyadmin_db 'Dev DB' do
   host '127.0.0.1'
@@ -27,14 +58,7 @@ phpmyadmin_db 'Dev DB' do
   password ''
   hide_dbs %w{ information_schema mysql phpmyadmin performance_schema }
 end
-
-bash "disable_firewall" do
-  only_if "chkconfig --list | grep iptables"
-  code <<-EOH
-    service iptables stop
-    chkconfig iptables off
-  EOH
-end
+  
 
 directory "/etc/php.d" do
   owner "root"
@@ -53,8 +77,6 @@ template "/etc/php.d/webapp.ini" do
   })
 end
 
-# Vagrant has a speical kernel, which cannot be updated with yum
-execute "echo 'exclude=kernel*' >> /etc/yum.conf"
 
 web_app "web_app" do
    docroot "/vagrant"
@@ -63,4 +85,3 @@ web_app "web_app" do
    server_aliases [node[:hostname], "wepapp.dev"]
    notifies :reload, resources(:service => "apache2"), :delayed
  end
-
